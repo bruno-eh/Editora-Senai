f1e291df959da4a417f781615e534c1c9e6102c2
Bruno F de Souza <bruno-eh@hotmail.com>
2021-09-20 20:35:19 -0300

    Commit build folder

---
+++ build/precache-manifest.02dd0cc19c9ce0d5f22c67ad2cd3aa86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22581ca3f40548e1e1fbf714f936e887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120fb69ee0c4844d65fb",
+    "url": "/static/css/main.618940e1.chunk.css"
+  },
+  {
+    "revision": "0e407eea34d1855cfb2c",
+    "url": "/static/js/2.93db4c25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93db4c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120fb69ee0c4844d65fb",
+    "url": "/static/js/main.3c0822b3.chunk.js"
+  },
+  {
+    "revision": "e9419607096510474d1a",
+    "url": "/static/js/runtime-main.81c04eb7.js"
+  }
+]);
